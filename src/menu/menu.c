9c07e95294ad036380e58eea7b7bcc756e3e2235
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-03-05 19:40:30 +0100

    Update: Code optimization of menu display

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -7,22 +7,28 @@
 
 #include "my_world.h"
 
+static sfBool should_close_window(win_settings_t win_settings)
+{
+    if (!sfRenderWindow_isOpen(win_settings.window))
+        return (sfTrue);
+    if (sfKeyboard_isKeyPressed(sfKeySpace))
+        return (sfTrue);
+    if (sfRenderWindow_pollEvent(win_settings.window, &win_settings.event)
+        && win_settings.event.type == sfEvtClosed)
+        return (sfTrue);
+    return (sfFalse);
+}
+
 static sfBool run_menu(win_settings_t win_settings)
 {
-    static sfEvent event;
-
     sfMusic_setVolume(win_settings.main_track, 75);
     sfView_reset(win_settings.view, (sfFloatRect){0, 0, 1920, 1080});
     while (sfKeyboard_isKeyPressed(sfKeyEnter));
-    sfRenderWindow_clear(win_settings.window, sfBlack);
-    sfRenderWindow_display(win_settings.window);
     while (!sfKeyboard_isKeyPressed(sfKeyEnter)) {
-        if (!sfRenderWindow_isOpen(win_settings.window))
+        if (should_close_window(win_settings))
             return (sfFalse);
-        if (sfKeyboard_isKeyPressed(sfKeySpace))
-            return (sfFalse);
-        if (sfRenderWindow_pollEvent(win_settings.window, &event) && event.type == sfEvtClosed)
-            return (sfFalse);
+        sfRenderWindow_clear(win_settings.window, sfBlack);
+        sfRenderWindow_display(win_settings.window);
     }
     return (sfTrue);
 }
